68c798455c09935ff31c6aa49998d3ce30c87f40
Abel Ngara <134216536+Enabletheunable@users.noreply.github.com>
2024-08-20 09:22:17 +0000

    added index page

---
+++ hello-world/app/index.jsx
@@ -0,0 +1,14 @@
+import{View, Text, SafeAreaView} from 'react-native'
+
+const HelloWorld = () =>{
+    return(
+        <SafeAreaView style={{
+            backgroundColor: '#8c1483'
+        
+        }}>
+            <Text> Hello world</Text>
+        </SafeAreaView>
+    )
+}
+export default HelloWorld
+
